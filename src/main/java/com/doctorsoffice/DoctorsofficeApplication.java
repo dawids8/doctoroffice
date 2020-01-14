5e32bce30681e0f10a8c382f7aaf846babdf743b
dawid <dawids8788@gmail.com>
2020-01-14 18:17:11 +0100

    init

---
+++ src/main/java/com/doctorsoffice/DoctorsofficeApplication.java
@@ -0,0 +1,13 @@
+package com.doctorsoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorsofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doctorsoffice/DoctorsofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorsoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
