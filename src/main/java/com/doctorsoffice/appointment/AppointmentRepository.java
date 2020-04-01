fd040d953f2a96f205688a89879974461c6b7f52
dawid <dawids8788@gmail.com>
2020-04-01 13:43:50 +0200

    New methods AppointmentRepository

--- src/main/java/com/doctorsoffice/appointment/AppointmentRepository.java
+++ src/main/java/com/doctorsoffice/appointment/AppointmentRepository.java
@@ -1,9 +1,19 @@
 package com.doctorsoffice.appointment;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
+    List<Appointment> findAllByDoctorId(Long id);
+
+    List<Appointment> findAllByPatientId(Long id);
+
+    @Override
+    @EntityGraph(value = "Appointment.detail", type = EntityGraph.EntityGraphType.LOAD)
+    List<Appointment> findAll();
 }
