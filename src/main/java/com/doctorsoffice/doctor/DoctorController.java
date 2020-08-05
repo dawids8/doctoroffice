9ed0abeb24f15d6b56e5051c81aae9f9b4e95155
dawid <dawids8788@gmail.com>
2020-08-05 20:50:34 +0200

    Create doctor method deleted

--- src/main/java/com/doctorsoffice/doctor/DoctorController.java
+++ src/main/java/com/doctorsoffice/doctor/DoctorController.java
@@ -21,19 +21,6 @@ public class DoctorController {
         this.doctorMapper = doctorMapper;
     }
 
-    @PostMapping("/create")
-    public DoctorDto create(@Valid @RequestBody DoctorDto doctorDto) {
-        try {
-            final Doctor doctor = doctorMapper.fromDto(doctorDto);
-
-            doctorService.create(doctor);
-
-            return doctorMapper.toDto(doctor);
-        } catch (DataIntegrityViolationException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
-        }
-    }
-
     @GetMapping("/get")
     public DoctorDto get(@RequestParam Long id) {
         try {
